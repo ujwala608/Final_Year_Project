48f2c37072448a40ae1a20a07588130784487ed6
Ujjwala Sable <67739077+ujwala608@users.noreply.github.com>
2023-01-29 14:33:41 +0530

    Add files via upload

---
+++ controller.py
@@ -0,0 +1,15 @@
+from pyfirmata import Arduino,SERVO
+
+port='COM7'
+pin=10
+
+board=Arduino(port)
+board.digital[pin].mode=SERVO
+def rotateServo(pin,angle):
+	board.digital[pin].write(angle)
+
+def doorAutomate(val):
+	if val==0:
+		rotateServo(pin, 180)
+	elif val==1:
+		rotateServo(pin, 40)
